fbd7f3c65db48e1dab39269843b58ed6e834c874
Yogeshdagade <dagadeyogesh5@gmail.com>
2025-07-31 10:33:25 +0530

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,12 +1,1 @@
-dict={'mon':1,'sun':2,'tue':3,}
-print("The given dictonary:",dict)
-check_key=input("Enter key to check:")
-check_value= input("Enter value:")
-if check_key in dict:
-    print(check_key,"is present.")
-    dict.pop(check_key)
-    dict[check_key]=check_value
-else:
-    print(check_key,"is not present")
-    dict[check_key]=check_value
-    print("Updated dictionary: ",dict)
+
